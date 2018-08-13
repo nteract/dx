3f1f89a74099929bbe35021383f6b9f77c000bf4
Kyle Kelley <rgbkrk@gmail.com>
2018-08-13 15:50:54 -0700

    quickly get to samples

--- dx/__init__.py
+++ dx/__init__.py
@@ -1,12 +1,10 @@
-
 from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
 
+from .datasets import load_sample
 import pandas as pd
 
 def dx(dataframe):
     with pd.option_context('display.html.table_schema', True):
         display(dataframe)
-
-from .datasets import load_sample
