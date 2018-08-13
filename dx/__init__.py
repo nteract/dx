4022a5f2bcfa94e7cb22f56ebd17a0a6b948a374
Kyle Kelley <rgbkrk@gmail.com>
2018-08-13 15:43:28 -0700

    display dataframe in the data explorer with dx(df)

--- dx/__init__.py
+++ dx/__init__.py
@@ -2,3 +2,7 @@
 from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
+
+def dx(dataframe):
+    with pd.option_context('display.html.table_schema', True):
+        display(dataframe)
