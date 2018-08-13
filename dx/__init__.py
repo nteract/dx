829b89aa1059af727b44b7648d7d9a32deae382a
Kyle Kelley <rgbkrk@gmail.com>
2018-08-13 15:49:28 -0700

    introduce a sample dataset

--- dx/__init__.py
+++ dx/__init__.py
@@ -3,6 +3,10 @@ from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
 
+import pandas as pd
+
 def dx(dataframe):
     with pd.option_context('display.html.table_schema', True):
         display(dataframe)
+
+from .datasets import load_sample
---
+++ dx/datasets.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+def load_sample():
+    return pd.read_csv("https://gist.githubusercontent.com/rgbkrk/a7984a8788a73e2afb8fd4b89c8ec6de/raw/db8d1db9f878ed448c3cac3eb3c9c0dc5e80891e/2015.csv")
