b076de05628b53f3815327003494fcfdb9e9fee1
Carol Willing <carolcode@willingconsulting.com>
2021-06-25 15:06:26 -0700

    add requirements and apply black

--- dx/__init__.py
+++ dx/__init__.py
@@ -1,26 +1,34 @@
 from ._version import get_versions
-__version__ = get_versions()['version']
+
+__version__ = get_versions()["version"]
 del get_versions
 
-from .datasets import load_sample
 import pandas as pd
 
+from .datasets import load_sample
+
+
 def dx(dataframe, sampled=None):
-    # All the metadata keys that we'll apply for just the data explorer media type, application/vnd.dataresource+json
+    """Create dx from a dataframe"""
+    # All the metadata keys that we'll apply for just the data explorer media
+    # type, `application/vnd.dataresource+json`
     scoped_metadata = {}
 
     if sampled:
         scoped_metadata["sampled"] = sampled
 
-    metadata = {
-        "application/vnd.dataresource+json": scoped_metadata
-    }
+    metadata = {"application/vnd.dataresource+json": scoped_metadata}
 
-    with pd.option_context('display.html.table_schema', True):
-        display(dataframe, metadata=metadata)
+    pd.set_option("display.html.table_schema", True)
+    with pd.option_context():
+        pd.options.display(dataframe, metadata=metadata)
+
 
 def enable():
+    """Enable html table display"""
     pd.options.display.html.table_schema = True
 
+
 def disable():
+    """Disable html table display"""
     pd.options.display.html.table_schema = False
--- dx/datasets.py
+++ dx/datasets.py
@@ -1,5 +1,8 @@
 import pandas as pd
 
+
 def load_sample():
-    df = pd.read_csv("https://gist.githubusercontent.com/rgbkrk/a7984a8788a73e2afb8fd4b89c8ec6de/raw/db8d1db9f878ed448c3cac3eb3c9c0dc5e80891e/2015.csv")
+    df = pd.read_csv(
+        "https://gist.githubusercontent.com/rgbkrk/a7984a8788a73e2afb8fd4b89c8ec6de/raw/db8d1db9f878ed448c3cac3eb3c9c0dc5e80891e/2015.csv"
+    )
     return df.set_index("Country")
